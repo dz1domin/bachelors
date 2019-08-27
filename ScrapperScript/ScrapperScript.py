b655d945f4dac6b3fea6f0a9586f46dcd9ce3d14
Dominik <dz1domin@gmail.com>
2019-08-27 17:04:09 +0200

    some improvements to previous design

--- ScrapperScript/ScrapperScript.py
+++ ScrapperScript/ScrapperScript.py
@@ -1,13 +1,34 @@
+# @author Dominik Dziuba
+
 import requests
-import json
+import urllib.parse
 
 k = 0
 
-with open('searchQuery.json', 'r') as file:
-    data = json.load(file)
-    for el in data['hits']:
-        img = el
-        imgURI = img["previewURL"]
+# example version if one wants to manually pass API response for downloading images
+# with open('exampleSearchQuery.json', 'r') as file:
+#     data = json.load(file)
+#     for el in data['hits']:
+#         img = el
+#         imgURI = img["previewURL"]
+#         imgURI = imgURI.replace(' ', '')[:-8].upper() + '_960_720.jpg'
+#         imgURI = imgURI.lower()
+#         print(imgURI)
+#         res = requests.get(imgURI)
+#         outFile = open('img_' + str(k) + '.jpg', 'wb')
+#         outFile.write(res.content)
+#         k += 1
+#         res.close()
+
+apiKey = input('paste your pixabay API key:\n')
+while True:
+    query = input('type your query in:\n')
+    myURL = 'https://pixabay.com/api/?' + urllib.parse.urlencode({'key': apiKey, 'q': query, 'image_type': 'photo'})
+    print('final URL: ' + str(myURL) + '\n')
+    res = requests.get(myURL)
+    data = res.json()
+    for hit in data['hits']:
+        imgURI = hit["previewURL"]
         imgURI = imgURI.replace(' ', '')[:-8].upper() + '_960_720.jpg'
         imgURI = imgURI.lower()
         print(imgURI)
@@ -16,4 +37,3 @@ with open('searchQuery.json', 'r') as file:
         outFile.write(res.content)
         k += 1
         res.close()
-    print(len(data['hits']))
