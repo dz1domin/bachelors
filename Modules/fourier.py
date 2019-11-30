3160f453a87208b9245901b11a08e452add9bf7c
Dominik <dz1domin@gmail.com>
2019-11-30 23:31:44 +0100

    some changes

--- Modules/fourier.py
+++ Modules/fourier.py
@@ -7,9 +7,10 @@ def fourier(img_path, options):
     img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     row, cols = img_gray.shape
     crow, ccol = row // 2, cols // 2
+    xoff, yoff = int(row * 0.05), int(cols * 0.05)
     fft = np.fft.fft2(img_gray)
     fft_shifted = np.fft.fftshift(fft)
-    fft_shifted[crow - 75:crow + 75, ccol - 75:ccol + 75] = 0
+    fft_shifted[crow - xoff:crow + xoff, ccol - yoff:ccol + yoff] = 0
     fft_inverse_shift = np.fft.ifftshift(fft_shifted)
     fft_inverse = np.fft.ifft2(fft_inverse_shift)
     fft_inverse = 20 * np.log(np.abs(fft_inverse))
