43f84da8a7cf91e6ec2b1d0bc22a4ad532aa2be5
Dominik <dz1domin@gmail.com>
2019-12-29 12:21:19 +0100

    error checking in modules

--- Modules/fourier/fourier.py
+++ Modules/fourier/fourier.py
@@ -15,5 +15,5 @@ def fourier(img_path, options):
     fft_shifted[crow - xoff:crow + xoff, ccol - yoff:ccol + yoff] = 0
     fft_inverse_shift = np.fft.ifftshift(fft_shifted)
     fft_inverse = np.fft.ifft2(fft_inverse_shift)
-    fft_inverse = 20 * np.log(np.abs(fft_inverse))
+    fft_inverse = np.log(np.abs(fft_inverse))
     return img_path, str(np.mean(fft_inverse) < float(options['thresh']))
