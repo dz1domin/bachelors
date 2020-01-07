7ae2b1c5ae779cca1c4c0c1975507bd90888cdfb
Kavaldrin <turbo3001997@gmail.com>
2020-01-07 01:26:41 +0100

    improvement of scripts

--- docker/start.sh
+++ docker/start.sh
@@ -1,16 +1,47 @@
 #!/bin/bash
 
+
+clean=false
+interactive=false
+
+for option in "$@"
+do
+case $option in 
+    -i|--interactive)
+    interactive=true
+    shift
+    ;;
+    -c|--clean)
+    clean=true
+    shift
+    ;;
+    *)
+    
+    ;;
+esac
+done
+
+
 echo "Building image..."
 imageHash=$(docker build .)
 echo "Image has been built"
 imageHash=$(echo "$imageHash" | grep -E -o 'Successfully built [0-9a-f]+' | cut -d' ' -f3)
+
 echo "Running container..."
 docker run "${imageHash}"
 echo "Container finished his job"
 echo "Copying result files to current directory..."
 containerHash=$(docker ps -aqf "ancestor=${imageHash}" | head -qn 1)
-echo "Copied"
-echo "Removing container and image"
 docker cp "${containerHash}:/root/ImageClassifier/bachelors-master" "result"
-docker rm "$(docker ps -aqf "ancestor=${imageHash}")"
-docker rmi "${imageHash}"
+echo "Copied"
+
+if $interactive; then
+    echo "Interactive mode"
+    docker run -it --entrypoint /bin/bash "${imageHash}"
+fi
+
+if $clean; then
+    echo "Removing container and image"
+    docker rm -f "$(docker ps -aqf "ancestor=${imageHash}")"
+    docker rmi -f "${imageHash}"
+fi
